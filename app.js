25920ec836ace99e04e52629216be23994ac5e8b
mohamad fawaz <mohammadfawaz261@gmail.com>
2023-05-15 14:16:23 +0300

    cahnges

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(logger("tiny"));
 app.use(
   cors({
-    origin: [ "https://rent-your-house.netlify.app"],
+    origin: [ "https://renting-a-house.netlify.app"],
     // credentials: true,
   })
 );
